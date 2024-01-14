4629885b08e80d5dcc2cc66bb294ec110546890d
lbzinuse75 <lbzinuse@hotmail.com>
2024-01-13 20:59:39 -0700

    individual add item fix and calculator

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,20 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+
+// const products = getLocalStorage('so-cart') ?? [];
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  const currentCart = getLocalStorage('so-cart') || [];
+  currentCart.push(product);
+  setLocalStorage('so-cart', currentCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
+
   const product = await dataSource.findProductById(e.target.dataset.id);
+  // products.push(product);
   addProductToCart(product);
 }
 
