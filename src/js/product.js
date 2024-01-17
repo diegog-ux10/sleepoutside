e2ece8e0427a0db729b34ea6c9bd6e81bfdafea1
LaurenSwartz <swa21010@byui.edu>
2024-01-17 14:24:25 +0200

    new

---
+++ src/js/product.js
@@ -0,0 +1,20 @@
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+
+const dataSource = new ProductData('tents');
+
+function addProductToCart(product) {
+  const currentCart = getLocalStorage('so-cart') || [];
+  currentCart.push(product);
+  setLocalStorage('so-cart', currentCart);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
