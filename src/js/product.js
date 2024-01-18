74fed48fee281790de60e2345b96626f22962710
lbzinuse75 <lbzinuse@hotmail.com>
2024-01-18 13:14:31 -0700

    Team activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,15 @@
-import { setLocalStorage } from './utils.mjs';
+import { setLocalStorage, getParams } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productId = getParams('product');
+console.log(productId);
+dataSource.findProductById(productId).then(res => console.log(res))
+
+
 
 // add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// document
+  // .getElementById('addToCart')
+  // .addEventListener('click', addToCartHandler);
