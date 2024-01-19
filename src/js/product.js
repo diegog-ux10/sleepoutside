0d3e5897e11092f1ea6e10ca86d6a777dd607e44
lbzinuse75 <lbzinuse@hotmail.com>
2024-01-19 12:34:35 -0700

    Lint and Format

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,9 @@
-import { getParams } from './utils.mjs';
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
+import { getParams } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData('tents');
-const productId = getParams('product');
+const dataSource = new ProductData("tents");
+const productId = getParams("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
@@ -11,9 +11,7 @@ product.init();
 // console.log(productId);
 // dataSource.findProductById(productId).then(res => console.log(res))
 
-
-
 // add listener to Add to Cart button
 // document
-  // .getElementById('addToCart')
-  // .addEventListener('click', addToCartHandler);
+// .getElementById('addToCart')
+// .addEventListener('click', addToCartHandler);s
