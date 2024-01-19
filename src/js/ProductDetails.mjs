57c70f40a76ff6652454a3d5e82f2e3e3c0810be
lbzinuse75 <lbzinuse@hotmail.com>
2024-01-19 12:14:07 -0700

    Attempt to finish Team 2

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,35 +1,55 @@
-export default class ProductData {
-    constructor(category) {
-      this.category = category;
-      this.path = `../json/${this.category}.json`;
-    }
-    getData() {
-      return fetch(this.path)
-        .then(convertToJson)
-        .then((data) => data);
-    }
-    async findProductById(id) {
-      const products = await this.getData();
-      return products.find((item) => item.Id === id);
-    }
-  }
+import { setLocalStorage } from "./utils.mjs";
+
+function productDetailsTemplate(product) {
+  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${product.Image}"
+      alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
+}
 
 export default class ProductDetails {
-    constructor(productId, dataSource) {
-        this.productId = productId;
-        this.product = {};
-        this.dataSource = dataSource;
-    }
-}
-    init();{};
+  constructor(productId, dataSource) {
+      this.productId = productId;
+      this.product = {};
+      this.dataSource = dataSource;
+  }
 
-    
-    function addProductToCart(product) {
-        setLocalStorage('so-cart', product);
-      }
-      // add to cart button event handler
-    //   async function addToCartHandler(e) {
-    //     const product = await dataSource.findProductById(e.target.dataset.id);
-        // addProductToCart(product);
-    }
+  async init() {
+  this.product = await this.dataSource.findProductById(this.productId);
+  this.renderProductDetails("main");
+  document
+    .getElementById("addToCart")
+    .addEventListener("click", this.addToCart.bind(this));
+  }
+  
+  addToCart() {
+    setLocalStorage("so-cart", this.product);
+  }
+
+  // function addProductToCart(product) {
+  //   setLocalStorage('so-cart', product);
+  // }
+    // add to cart button event handler
+  //   async function addToCartHandler(e) {
+  //     const product = await dataSource.findProductById(e.target.dataset.id);
+      // addProductToCart(product);
+  
+  renderProductDetails(selector) {
+    const element = document.querySelector(selector);
+    element.insertAdjacentHTML(
+      "afterBegin",
+      productDetailsTemplate(this.product)
+    );
+  }
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,15 @@
-import { setLocalStorage, getParams } from './utils.mjs';
+import { getParams } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-// const products = getLocalStorage('so-cart') ?? [];
-
-const dataSource = new ProductData("tents");
-
+const dataSource = new ProductData('tents');
 const productId = getParams('product');
-console.log(productId);
-dataSource.findProductById(productId).then(res => console.log(res))
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+// console.log(productId);
+// dataSource.findProductById(productId).then(res => console.log(res))
 
 
 
