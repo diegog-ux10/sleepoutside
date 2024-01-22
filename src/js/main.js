96369d2a9a885bd733868952fc1468867b9f77db
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-01-22 17:29:25 -0400

    trying to make it work cart details

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
-
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const productList = new ProductList("tents", dataSource, element);
-
-productList.init();
