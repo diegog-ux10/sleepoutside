3836e88e77cb07a62e479f1702659beb34d543a2
butros01 <butrosmbuvi@gmail.com>
2024-01-25 13:36:07 +0300

    solve broken cart

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,4 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 
 function productDetailsTemplate(product) {
   return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
@@ -18,6 +18,17 @@ function productDetailsTemplate(product) {
     </div></section>`;
 }
 
+// function addProductToCart(product) {
+//   const currentCart = getLocalStorage("so-cart") || [];
+//   currentCart.push(product);
+//   setLocalStorage("so-cart", currentCart);
+// }
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addToCart(product);
+}
+
 export default class ProductDetails {
   constructor(productId, dataSource) {
     this.productId = productId;
@@ -27,25 +38,35 @@ export default class ProductDetails {
   async init() {
     // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
+    console.log(this.product)
     // once we have the product details we can render out the HTML
-    this.renderProductDetails("main");
+    this.renderProductDetails('main');
     // once the HTML is rendered we can add a listener to Add to Cart button
     // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
     document
-      .getElementById("addToCart")
-      .addEventListener("click", this.addToCart.bind(this));
+      .getElementById('addToCart')
+      .addEventListener('click', this.addToCart.bind(this));
   }
   
-  addToCart() {
-    setLocalStorage("so-cart", this.product);
-  }
-
-
   renderProductDetails(selector) {
     const element = document.querySelector(selector);
     element.insertAdjacentHTML(
-      "afterBegin",
+      'afterBegin',
       productDetailsTemplate(this.product)
     );
   }
+  addToCart() {
+    // Retrieve the existing cart items from local storage
+  const existingCart = getLocalStorage('so-cart') || [];
+
+  // Ensure that existingCart is an array
+  const products = Array.isArray(existingCart) ? existingCart : [];
+
+  // Push the current product into the array
+  products.push(this.product);
+
+  // Update the local storage with the modified array
+  setLocalStorage('so-cart', products);
+  }
+  
 }
