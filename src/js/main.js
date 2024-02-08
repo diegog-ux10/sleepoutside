c154f3b6385e8268da0cbe76523f1265436347a7
leonah01 <don22010@byui.edu>
2024-02-08 16:59:55 +0200

    newsletter
    Signed-off-by: leonah01 <don22010@byui.edu>

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,41 @@
 import { loadHeaderFooter } from './utils.mjs';
 
 loadHeaderFooter();
+
+const formSubmitted = sessionStorage.getItem('formSubmitted');
+
+if (formSubmitted) {
+  // Display message
+  document.getElementById('subscriptionMessage').style.display = 'block';
+
+  // Clear only the 'formSubmitted' item from sessionStorage
+  sessionStorage.removeItem('formSubmitted');
+}
+
+document
+  .getElementById('subscribeButton')
+  .addEventListener('click', function () {
+    // Display the subscription message
+    document.getElementById('subscriptionMessage').style.display = 'block';
+
+    // Set a flag in sessionStorage indicating that the form was submitted
+    sessionStorage.setItem('formSubmitted', 'true');
+  });
+
+// Handle page refresh
+window.addEventListener('beforeunload', function () {
+  // Clear only the 'formSubmitted' item from sessionStorage on page refresh
+  sessionStorage.removeItem('formSubmitted');
+});
+
+document
+  .getElementById('createBtn')
+  .addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    // Display a message
+    var message = 'Successfully, signed-up!';
+
+    // Update the message div with the message
+    document.getElementById('message').textContent = message;
+  });
